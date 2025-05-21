a4c811235139b175f211603f18f59326fd3a637a
abhishek-sharma-8412 <153045660+abhishek-sharma-8412@users.noreply.github.com>
2025-05-21 18:51:04 +0530

    Initial commit of exam-proctoring-app code

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import StudentLogin from './pages/StudentLogin';
+import StudentSignIn from './pages/StudentSignIn';
+import AdminLogin from './pages/AdminLogin';
+import AdminSignIn from './pages/AdminSignIn';
+import GlobalStyles from './styles/GlobalStyles';
+import ExamInterface from './components/exam/ExamInterface';
+import ProctorDashboard from './components/dashboard/ProctorDashboard';
+import ProtectedRoute from './components/ProtectedRoute';
+function App() {
+  return (
+    <Router>
+      <GlobalStyles />
+         <Routes>
+        <Route path="/student-login" element={<StudentLogin />} />
+        <Route path="/student-signin" element={<StudentSignIn />} />
+        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/admin-signin" element={<AdminSignIn />} />
+        <Route
+          path="/exam"
+          element={
+            <ProtectedRoute role="student">
+              <ExamInterface />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/proctor"
+          element={
+            <ProtectedRoute role="admin">
+              <ProctorDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/" element={<Navigate to="/student-login" replace />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
